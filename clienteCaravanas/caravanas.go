9065fe0f2f1f6dc5052bbf9a84f153c273b36a75
felipefferrada <felipe.ferrada@usm.cl>
2024-09-09 03:04:32 -0300

    instrucciones comentadas

--- clienteCaravanas/caravanas.go
+++ clienteCaravanas/caravanas.go
@@ -21,6 +21,40 @@ func main() {
 	client := pb.NewMessageServiceClient(conn)
 
 	// Enviar un mensaje al servidor
+
+	/*
+		Las caravanas son las encargadas de realizar la entrega de los suministros en las llanuras de Eidolon.
+		Como se mencion´o anteriormente, hay tres caravanas: dos dedicadas a los ostronitas y una destinada
+		al transporte general de suministros Grineer y otros.
+
+		Las caravanas permanecen en Cetus hasta que reciben alg´un paquete para entregar.
+
+		Las caravanas esperan por un segundo paquete para entregar durante un periodo de tiempo
+		definido al inicializar el sistema
+
+		La caravana general, encargada de las entregas restantes, prioriza los paquetes prioritarios
+		sobre los normales.
+
+		Las caravanas siempre entregan primero el paquete que genere mayores ingresos para Konzu,
+		a menos que el paquete no sea recibido. En tal caso, se intentar´a con el siguiente paquete y
+		luego se intentar´a nuevamente entregar el paquete que no pudo ser entregado.
+
+		Se considera una entrega completa cuando la caravana regresa a Cetus, incluso si no pudo
+		entregar todos los paquetes.
+
+		La caravana informa a Cetus cuando se completa una entrega, independientemente de si fue
+		recibida o no
+
+		Cada caravana lleva un registro de su funcionamiento, detallando cada paquete que tiene y ha
+		entregado. Espec´ıficamente, ID del paquete, tipo de paquete, valor, escolta, destino, n´umero
+		de intentos y fecha de entrega.
+
+		Para las entregas realizadas por las caravanas existe un porcentaje de ´exito asociado a si se los
+		suministro se logran entregar o se falla la misi´on, en este caso, existe un 85% de probabilidad de que
+		se complete con ´exito la entrega de suministros al frente.
+
+	*/
+
 	message := "Hola, servidor!"
 	if len(os.Args) > 1 {
 		message = os.Args[1]
--- clienteFacciones/facciones.go
+++ clienteFacciones/facciones.go
@@ -21,6 +21,29 @@ func main() {
 	client := pb.NewMessageServiceClient(conn)
 
 	// Enviar un mensaje al servidor
+
+	/*
+		Los clientes son quienes realizan las ordenes de suministros
+
+		Generan ordenes periodicamente basadas en las necesidades de suministros de los Ostronitas o
+		los Grineer, las cuales se envıan al sistema logıstico para su procesamiento. Para este proceso,
+		se debe utilizar Protocol Buffers (ver gRPC).
+
+		Los clientes pueden solicitar el estado de sus envıos ingresando y enviando su codigo de
+		seguimiento al sistema logıstico, que les devolvera el estado de su pedido segun sus registros.
+		Para este intercambio de informacion, se puede emplear cualquier medio de comunicacion entre
+		el sistema logıstico y el cliente que sea confiable.
+
+		Al momento de instanciarse cada sistema se requerir´a ingresar los tiempos bajo los cuales van
+		a operar, ya sea el tiempo de espera de la caravana o cu´anto se demora en enviar cada paquete,
+		y el tiempo de espera entre el env´ıo de ´ordenes en el cliente. Respecto a este ´ultimo, tambi´en
+		debe solicitar el comportamiento a seguir (Ostronita/Grineer).
+
+		Se debe generar una rutina para que simule el comportamiento de los clientes, considerando
+		el env´ıo de ´ordenes y la solicitud del estado de un paquete.
+
+	*/
+
 	message := "Hola, servidor!"
 	if len(os.Args) > 1 {
 		message = os.Args[1]
